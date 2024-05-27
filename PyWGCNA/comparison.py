7d622f6c4335a3025423fc9b3b8ccc83d155d806
helix184 <dangminhnguyet09@gmail.com>
2024-05-27 09:15:35 +0000

    test app

--- PyWGCNA/comparison.py
+++ PyWGCNA/comparison.py
@@ -441,15 +441,14 @@ class Comparison:
 
         # Remove the legend and add a colorbar
         ax.get_legend().remove()
-        ax.figure.colorbar(sm, shrink=0.25, label='-log10(P_value)')
-
+        ax.figure.colorbar(sm, shrink=0.25, label='-log10(P_value)', ax=plt.gca())
         handles, labels = ax.get_legend_handles_labels()
         entries_to_skip = labels.index('size') + 1
         handles = handles[entries_to_skip:]
         labels = labels[entries_to_skip:]
-        for h in handles[1:]:
-            sizes = [s for s in h.get_sizes()]
-            h.set_sizes(sizes)
+        # for h in handles[1:]:
+        #     sizes = [s for s in h.get_sizes()]
+        #     h.set_sizes(sizes)
         labels = labels[:1] + [f'{float(lab):.2f}' for lab in labels[1:]]
         legend_size = ax.legend(handles, labels,
                                 title=bubble_size,
